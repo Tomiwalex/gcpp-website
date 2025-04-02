f42160189ee29b7d6278ead320a8689633438840
AL£X <98038873+Tomiwalex@users.noreply.github.com>
2025-04-02 07:35:03 +0100

    Update Header.tsx

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -7,6 +7,7 @@ import { Button } from '../ui/button';
 import { usePathname } from 'next/navigation';
 import { Icon } from '@iconify/react/dist/iconify.js';
 import { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const Header = () => {
   const pathname = usePathname();
@@ -31,9 +32,13 @@ const Header = () => {
   ];
 
   return (
-    <header className="sticky -top-12 bg-[#14120F]">
+    <header
+      className={`sticky ${isMenuOpen ? 'top-0' : '-top-12'} bg-[#14120F]`}
+    >
       <div className="mx-auto w-full max-w-[1512px]">
-        <div className="flex items-center justify-end gap-x-6 border-b-[0.8px] border-b-[#1E1E1E] px-5 py-3 font-normal text-[#ACACAC]">
+        <div
+          className={`${isMenuOpen ? 'hidden lg:flex' : 'flex'} items-center justify-end gap-x-6 border-b-[0.8px] border-b-[#1E1E1E] px-5 py-3 font-normal text-[#ACACAC]`}
+        >
           <Link
             href="#"
             className="hidden text-xs transition-all duration-300 ease-in-out hover:text-[#EB9202] md:inline-block"
@@ -115,6 +120,45 @@ const Header = () => {
             </div>
           </div>
         </div>
+
+        <AnimatePresence>
+          {isMenuOpen && (
+            <motion.div
+              initial={{ opacity: 1 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              className="fixed top-[80px] left-0 z-[20] h-[calc(100dvh-80px)] w-[100dvw] bg-[#14120F30] backdrop-blur-md md:hidden"
+            >
+              <motion.div
+                initial={{ translateX: '30%' }}
+                exit={{ translateX: '30%' }}
+                animate={{ translateX: 0 }}
+                className="ml-auto h-full w-[60%] max-w-[300px] bg-[#14120F]"
+              >
+                <nav className="flex flex-col gap-7 p-7">
+                  {navList.map((link, index) => (
+                    <Link
+                      href={link.path}
+                      className={`cursor-pointer text-base ${
+                        location.pathname === link.path
+                          ? 'font-semibold text-[#EB9202]'
+                          : 'font-medium text-white'
+                      } relative`}
+                      key={index}
+                    >
+                      {link.name}
+                    </Link>
+                  ))}
+
+                  {/* schedule a consult */}
+                  <Button className="h-8 rounded px-4 py-2 text-[13px] font-medium transition-all duration-300 ease-in-out">
+                    Schedule a consult
+                  </Button>
+                </nav>
+              </motion.div>
+            </motion.div>
+          )}
+        </AnimatePresence>
       </div>
     </header>
   );
