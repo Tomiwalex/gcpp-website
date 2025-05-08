5b64d5128f4ab08cfa775c76a728cbc67cd06779
AL£X <98038873+Tomiwalex@users.noreply.github.com>
2025-05-08 08:52:14 +0100

    Update Authors.tsx

--- src/components/screens/authors/Authors.tsx
+++ src/components/screens/authors/Authors.tsx
@@ -4,6 +4,7 @@ import { members } from '@/components/data/members';
 import AuthordCard from './AuthordCard';
 import { useSearchParams } from 'next/navigation';
 import { Icon } from '@iconify/react/dist/iconify.js';
+import { Suspense } from 'react';
 
 const Authors = () => {
   const searchParams = useSearchParams();
@@ -12,20 +13,22 @@ const Authors = () => {
     member.name.toLowerCase().includes(search)
   );
   return (
-    <div>
-      <div className="grid grid-cols-2 gap-5 md:grid-cols-3 lg:grid-cols-5">
-        {filteredMembers.map(author => (
-          <AuthordCard author={author} key={author.name} />
-        ))}
-      </div>
-
-      {!filteredMembers.length && (
-        <div className="mx-auto my-20 flex max-w-[150px] flex-col items-center justify-center text-center">
-          <Icon className="text-5xl" icon={'lucide:search-x'} />
-          <p className="mt-3 font-medium text-gray-600">Author not found</p>
+    <Suspense fallback={'loading...'}>
+      <div>
+        <div className="grid grid-cols-2 gap-5 md:grid-cols-3 lg:grid-cols-5">
+          {filteredMembers.map(author => (
+            <AuthordCard author={author} key={author.name} />
+          ))}
         </div>
-      )}
-    </div>
+
+        {!filteredMembers.length && (
+          <div className="mx-auto my-20 flex max-w-[150px] flex-col items-center justify-center text-center">
+            <Icon className="text-5xl" icon={'lucide:search-x'} />
+            <p className="mt-3 font-medium text-gray-600">Author not found</p>
+          </div>
+        )}
+      </div>
+    </Suspense>
   );
 };
 
